284df08655b86587f15e1fbcd24ec04eeeee408a
Kangonaut <120662571+Kangonaut@users.noreply.github.com>
2026-09-09 13:04:49 +0200

    add utils

--- src/hirnkastl/utils.py
+++ src/hirnkastl/utils.py
@@ -1,7 +1,12 @@
 import base64
+import random
 from pathlib import Path
 
 import filetype
+
+
+def get_new_anki_id() -> int:
+    return random.randrange(1 << 30, 1 << 31)
 
 
 def file_to_base64(path: Path) -> str:
@@ -11,4 +16,6 @@ def file_to_base64(path: Path) -> str:
 
 def get_file_mime(path: Path) -> str:
     kind = filetype.guess(path)
+    if kind is None:
+        raise Exception(f"Cannot infer file type of {path}")
     return kind.mime
