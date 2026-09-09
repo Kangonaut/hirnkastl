358e3d02f4c496fc4bddb2417435a0b4196c1a83
Kangonaut <120662571+Kangonaut@users.noreply.github.com>
2026-09-09 13:04:59 +0200

    add constants

---
+++ src/hirnkastl/constansts.py
@@ -0,0 +1,7 @@
+from hirnkastl.cards import CodeTracingCard, GenericCard, MathCard
+
+CARD_CLASS_REGISTRY = {
+    "generic": GenericCard,
+    "math": MathCard,
+    "code-tracing": CodeTracingCard,
+}
